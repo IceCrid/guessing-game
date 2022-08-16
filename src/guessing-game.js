7c6edb21b3971d9eb30815abefdbe8253f404429
IceCrid <icecrid@gmail.com>
2022-08-17 02:58:16 +0300

    feat: guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.minNumber = min;
+      this.maxNumber = max;
     }
 
     guess() {
-
+      this.number = Math.round((this.maxNumber + this.minNumber) / 2);
+      return this.number;
     }
 
     lower() {
-
+      return this.maxNumber = Math.ceil(this.number);
     }
 
     greater() {
-
+      return this.minNumber = Math.floor(this.number);
     }
 }
 
